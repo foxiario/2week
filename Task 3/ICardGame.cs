a75fe7b46f1acafb5673478ad05e32eb45115b22
Egor Prokhorov <foxiario@gmail.com>
2022-10-08 04:41:32 +0300

    Забыл

---
+++ Task 3/ICardGame.cs
@@ -0,0 +1,9 @@
+﻿public interface ICardGame
+{
+    public int Quantity { get; }
+    public string Name { get; }
+    public void StartGame();
+
+    public delegate void WinHandler(int movesCount);
+    public event WinHandler WinEvent;
+}
